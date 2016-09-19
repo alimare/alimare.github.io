2592642bcbbe9cbb066f21b0a495c5867a3f3456
Devin Shurtleff <dshurt@gmail.com>
2016-09-18 23:55:25 -0600

    Update footer.php

--- main/footer.php
+++ main/footer.php
@@ -1,3 +1,3 @@
 <div style="color: black; text-align: center; width: 98%;" class="main-footer">
-	Alimar Enterainment &COPY; 1995 - <script type="text/javascript">document.write((new Date()).getFullYear()); </script>	
+	Alimar Enterainment &COPY; 1995 - <span id='cpyearId'></span></span> <script type="text/javascript">$('#cpyearId').html((new Date()).getFullYear()); </script>	
 </div>
